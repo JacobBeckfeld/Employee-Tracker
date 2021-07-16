67f3727f8922f7c02652b7f43f18a14a19577210
JacobBeckfeld <jacbeck456@gmail.com>
2021-07-16 18:39:23 -0500

    started an addEmployee function and added a viewAllEmployee function

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-
+const cTable = require('console.table');
 const {config} = require("./creds");
-const Choices = require("inquirer/lib/objects/choices");
 
 const connection = mysql.createConnection(config);
 
 
 
+connection.connect((err) =>{
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}\n`)
+})
 
-viewEmployees();
+const viewEmployees = () => {
+    connection.query("SELECT employee.first_name, employee.last_name, role.id, role.salary, department.name AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",(err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+        })
+};
 
-viewDepartment();
+// viewDepartment();
 
-viewByManager();
+// viewByManager();
 
-addEmployee();
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            name: "firstName",
+            type: "input",
+            message: "What is their first name?"
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "What is their last name?"
+        },
+        {
+            name: "role",
+            type: "rawlist",
+            message: "what is their role?",
+            choices: connection.query("SELECT * FROM role", (err,res) => {
+                let roleArray = [];
+                if (err) throw err
+                for (var i = 0; i < res,length; i++){
+                    roleArray.push(res[i].id)
+                }
+                return roleArray;
+            })
+        }
+    ])
+};
 
-removeEmployee();
+// updateEmployeeRole();
 
+// addRole();
 
-updateRole();
-
-
-
-updateManager();
+// addDepartment();
 
 
 const start = () => {
@@ -66,10 +98,5 @@ const start = () => {
         }
     })
 }
-
-connection.connect((err) =>{
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}\n`)
-})
 
 start();
